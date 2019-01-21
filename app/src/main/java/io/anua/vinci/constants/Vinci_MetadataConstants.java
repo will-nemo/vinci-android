eeb7c0a2b4c93023684d474d7a633f77a0801bb9
anua-software <anuasoftware@gmail.com>
2019-01-21 16:53:39 -0500

    Task/AVB-0002: Cleaned up activities and added docs

---
+++ app/src/main/java/io/anua/vinci/constants/Vinci_MetadataConstants.java
@@ -0,0 +1,11 @@
+package io.anua.vinci.constants;
+
+public class Vinci_MetadataConstants {
+
+    /**************************
+     * Constants
+     *************************/
+
+    public static String BASE_URL = "https://api.iextrading.com/1.0/";
+    public static String LOADING_DIALOG = "Loading....";
+}
